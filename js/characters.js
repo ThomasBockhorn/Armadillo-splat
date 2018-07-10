25554d9c67f9cbae5157babdc1dc35ca795de1cd
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-07-10 07:13:40 -0500

    Created the protagonist object and allowed user to use arrow keys

--- js/characters.js
+++ js/characters.js
@@ -1,100 +1,61 @@
 // Dynamically resize the canvas to be its CSS displayed size
 (window.onresize = function(){
-    const enemyPlane  = document.querySelector('#enemyPlane');
-    enemyPlane.width  = enemyPlane.offsetWidth;
-    enemyPlane.height = enemyPlane.offsetHeight;
+    const protagonistPlane  = document.querySelector('#protagonistPlane');
+    protagonistPlane.width  = protagonistPlane.offsetWidth;
+    protagonistPlane.height = protagonistPlane.offsetHeight;
 })();
 
+//global variables
+const protagonistPlane = document.getElementById("protagonistPlane");
+const protagonistCtx = protagonistPlane.getContext("2d");
 
-let requestID = undefined; //This global variable allows the animation to stop and start over
-let carArray = [] //This will hold the car objects for display
-
-//This function regulating the fps of the car image
-function animationTiming(){
-    characterTime.fps = 60;  //60fps is the ideal setting after some trial an error
-    characterTime.fpsInterval = 1000/characterTime.fps;
-    characterTime.then = window.performance.now();
-};
-
-//These global variables help make the animation more smooth
-const characterTime = {
-    frameCount: 0,
-    fps: 0,
-    fpsInterval: 0,
-    startTime: 0,
-    now: 0,
-    then: 0,
-    elapsed: 0
+//Protagonist object
+const protagonist = {
+    xPosition : 500,
+    yPosition : 400,
+    press: 0, //indicator if a key was pressed
+    keyPress: 0, //holds the value after a key was pressed
+    img: new Image()
 }
 
-//Creating the car class that will be the prototype for all the cars that 
-//will be displayed.  Each car will have a sensor that will detect if the 
-//car hit the armadillo
-class Cars{
-    constructor(carImg, yPosition, speed){
-        this.carImg = new Image();
-        this.carImg.src = carImg;
-        this.yPosition = yPosition;
-        this.enemyPlane = document.getElementById("enemyPlane");
-        this.ctx = this.enemyPlane.getContext("2d");
-        this.xPosition = this.enemyPlane.width;
-        this.speed = speed;  //This will change the speed of the car
+//keyMove is a function that will allow the user to move the armadillo with the arrow keys
+function keyMove(){
+    //loads the image and initial position 
+    protagonist.img.src = "pic/armadillo.png", //The image of the armadillo
+    protagonist.img.onload = function() {
+        protagonistCtx.drawImage(protagonist.img, protagonist.xPosition, protagonist.yPosition, 70, 70);
     }
-    //This create function will draw the cars onto the enemyPlane canvas.  It will
-    //use the time object as reference for a better animation
-    draw(){
-        const ref = this;
-        
-        //requests another frame
-        if(ref.xPosition > -50){
-            //Sees if enough time has elapsed
-            characterTime.now = window.performance.now();
-            characterTime.elapsed = characterTime.now - characterTime.then;
-            //if it did, we draw the frame
-            if(characterTime.elapsed > characterTime.fpsInterval){
-                //calculates then time stamp
-                characterTime.then = characterTime.now - (characterTime.elapsed % characterTime.fpsInterval);
-                //This clears the canvas so the cars seem to animate
-                ref.ctx.clearRect(0, 0, ref.enemyPlane.width, ref.enemyPlane.height);
-                ref.ctx.drawImage(ref.carImg, ref.xPosition, ref.yPosition, 80, 80);
-                ref.xPosition = ref.xPosition - ref.speed;
-            } 
-            requestID = requestAnimationFrame(function(){
-                ref.draw();
-            });   
-        }else{
-            cancelAnimationFrame(requestID);
-            requestID = undefined;
-            ref.xPosition = ref.enemyPlane.width
-            requestID = requestAnimationFrame(function(){
-                ref.draw();
-            });
+    //This gets the value of the keyPress when a key is pressed
+    document.onkeydown = function(e){
+        protagonist.press = 1;
+        protagonist.keyPress = window.event?e.keyCode:e.which;
+    }
+    //if the key is released it will change the value of press
+    document.onkeyup = function(e){
+        protagonist.press = 0;
+    }
+    //This will determine what key is pressed and if it is a arrow key, move the character according
+    setInterval(function(){
+        if(protagonist.press === 0){
+            return;  // if the key is released do nothing
         }
-    }
-}
-//this starts that animation clock
-animationTiming();
+        else if ((protagonist.keyPress === 37) && (protagonist.xPosition > 0)){
+            protagonist.xPosition -= 4;  //if the left arrow key is pressed
+        }
+        else if ((protagonist.keyPress === 38) && (protagonist.yPosition > 0)){
+            protagonist.yPosition -= 4;  //if up arrow key is pressed
+        }
+        else if ((protagonist.keyPress === 39) && (protagonist.xPosition < protagonistPlane.width - 70)){
+            protagonist.xPosition += 4;  //if right arrow key is pressed
+        }
+        else if ((protagonist.keyPress === 40) && (protagonist.yPosition < protagonistPlane.height - 70)){
+            protagonist.yPosition += 4; //if down arrow key is pressed
+        }
 
-function storeCharacter(){
-    //These are the three road positions on which the cars can drive on
-    //It creates the cars then puts them in an array for storage
-    for(let i = 0; i < 3; i++){
-        const roadPosition = [50, 150, 250];
-        const source = "pic/enemy_car.png"; //The same car image is loaded
-        const yPosition = roadPosition[i];
-        let speed = Math.floor(Math.random( )* 5 +1);
-
-        //This creates the car
-        const car = new Cars(source, yPosition, speed);
-
-        //Then we push it into the array
-        carArray.push(car);
-    }
-   
-}
-storeCharacter();
-for(let i = 0; i < carArray.length; i++){
-    carArray[i].draw();
+        //clears canvas then redraws image
+        protagonistCtx.clearRect(0,0, protagonistPlane.width, protagonistPlane.height);
+        protagonistCtx.drawImage(protagonist.img, protagonist.xPosition, protagonist.yPosition, 70, 70);
+    }, 20);
 }
 
-
+keyMove();
